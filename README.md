a3e92ce9f3562ee90aeb153ef6fa958c844d57d0
MadInc63 <MadInc63@gmail.com>
2018-02-22 09:18:22 +0400

    update

--- README.md
+++ README.md
@@ -1,7 +1,39 @@
 # Cinemas
 
-[TODO. There will be project description]
+This script searches for movies shown in the movie theater and displays the list of films with the highest rating.
 
+#How to use
+
+The script requires the installed Python interpreter version 3.5 To call the help, run the script with the -h or --help option.
+```Bash
+>python cinemas.py -h
+usage: cinemas.py [-h] [-count COUNT]
+
+Number of output films
+
+optional arguments:
+  -h, --help    show this help message and exit
+  -count COUNT  enter number of output films
+```
+To search for a movie, you can specify the number of movies to be displayed (the default is 10 movies)
+```Bash
+>cinemas.py -count 8
+Get a list of movies shown in the cinema from Afisha.ru...
+Get movies rating from Kinopoisk.ru...
+Collecting data:: 100%|██████████| 12/12 [00:16<00:00,  1.40s/it]
++------------------------------------------+----------+----------------+---------+
+| Movie title                              |   Rating |   Rating count |   Votes |
+|------------------------------------------+----------+----------------+---------|
+| Три билборда на границе Эббинга, Миссури |    8.304 |          46517 |      65 |
+| Движение вверх                           |    8.271 |          58562 |      79 |
+| Приключения Паддингтона-2                |    8.268 |           8132 |      65 |
+| Лед                                      |    7.136 |           6556 |     140 |
+| Все деньги мира                          |    6.833 |            321 |      96 |
+| Секретное досье                          |    6.597 |           1467 |      82 |
+| Дикий                                    |    5.566 |            908 |      58 |
+| Пятьдесят оттенков свободы               |    5.548 |           9296 |     112 |
++------------------------------------------+----------+----------------+---------+
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- cinemas.py
+++ cinemas.py
@@ -1,20 +1,88 @@
+import requests
+import argparse
+from tqdm import tqdm
+from tabulate import tabulate
+from bs4 import BeautifulSoup
 
 
-def fetch_afisha_page():
-    pass
+def parse_args():
+    parser = argparse.ArgumentParser(description='Number of output films')
+    parser.add_argument(
+        '-count',
+        default=10,
+        type=int,
+        help='enter number of output films')
+    return parser.parse_args()
+
+
+def fetch_page(url, params=None):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/64.0.3282.140 Safari/537.36',
+        'Accept-Language': 'ru,en;q=0.9'
+    }
+    return requests.get(url, params=params, headers=headers)
 
 
 def parse_afisha_list(raw_html):
-    pass
+    films_information = {}
+    min_showing_cinemas_count = 50
+    soup = BeautifulSoup(raw_html, 'html.parser')
+    tags = soup.find_all(class_='m-disp-table')
+    for tag in tags:
+        film_title = tag.h3.string
+        cinemas_count = len(
+            tag.next_sibling.next_sibling.find_all(class_='b-td-item')
+        )
+        if min_showing_cinemas_count < cinemas_count:
+            films_information[film_title] = cinemas_count
+    return films_information
 
 
-def fetch_movie_info(movie_title):
-    pass
+def fetch_movie_page(movie_title):
+    url = 'https://www.kinopoisk.ru/index.php'
+    params = {'kp_query': movie_title, 'first': 'yes', 'what': ''}
+    response = fetch_page(url, params)
+    return response
 
 
-def output_movies_to_console(movies):
-    pass
+def parse_film_rating(raw_html):
+    soup = BeautifulSoup(raw_html, 'html.parser')
+    try:
+        rating = soup.find(class_='rating_ball').string
+        rating_count = soup.find(
+            class_='ratingCount'
+        ).string.replace(u'\xa0', '')
+    except AttributeError:
+        rating = 0
+        rating_count = 0
+    return float(rating), int(rating_count)
+
+
+def output_movies_to_console(movies_list, number_of_output_films):
+    headers = ['Movie title', 'Rating', 'Rating count', 'Votes']
+    print(tabulate(
+        movies_list[:number_of_output_films], headers, tablefmt="psql"
+    ))
 
 
 if __name__ == '__main__':
-    pass
+    args = parse_args()
+    afisha_page_url = 'https://www.afisha.ru/msk/schedule_cinema/'
+    afisha_page_raw = fetch_page(afisha_page_url)
+    print('Get a list of movies shown in the cinema from Afisha.ru...')
+    showing_films = parse_afisha_list(afisha_page_raw.text)
+    print('Get movies rating from Kinopoisk.ru...')
+    movie_list_info = []
+    for movie_name, votes in tqdm(
+            showing_films.items(), desc='Collecting data:'
+    ):
+        movie_page = fetch_movie_page(movie_name)
+        movie_rating, movie_rating_count = parse_film_rating(movie_page.text)
+        movie_info = [movie_name, movie_rating, movie_rating_count, votes]
+        movie_list_info.append(movie_info)
+    sorted_movie_list = (sorted(
+        movie_list_info, key=lambda movie: movie[1], reverse=True
+    ))
+    output_movies_to_console(sorted_movie_list, args.count)
